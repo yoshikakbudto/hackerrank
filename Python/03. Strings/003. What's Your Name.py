45abaa61904152c58d2fdf6ea344b76479a2b417
Dmitry A. Esarev <dmitriy_yesarev@office.targem.ru>
2021-06-17 19:56:58 +0500

    strings: complete problems

--- Python/03. Strings/003. What's Your Name.py
+++ Python/03. Strings/003. What's Your Name.py
@@ -1,2 +1,5 @@
 # Problem: https://www.hackerrank.com/challenges/whats-your-name/problem
 # Score: 10
+
+def print_full_name(first, last):
+    print(f"Hello {first} {last}! You just delved into python.")
